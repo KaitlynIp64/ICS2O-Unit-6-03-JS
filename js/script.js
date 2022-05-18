037507f66310d6ddb1330b03875ab4b526266cb1
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-18 17:15:30 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-x-yy-JS/sw.js", {
-    scope: "/ICS2O-Unit-x-yy-JS/",
+  navigator.serviceWorker.register("/ICS2O-Unit-6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit-6-03-JS/",
   });
 }
 
