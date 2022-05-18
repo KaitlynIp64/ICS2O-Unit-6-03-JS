41e0eb50cb1b305ee2a955e27af616add8d4a6d5
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-18 17:29:14 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -14,7 +14,34 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * Input
- */
-function myButtonClicked() {
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
 
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getImage("https://api.weather.com/img")
